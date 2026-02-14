381a0b5cfe2e530074a5c1bebdd8aafe46e90b11
enocol <eno_col@yahoo.com>
2026-02-14 20:28:17 +0000

    setup backend with get, post and delete endpoints

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+import {neon} from "@neondatabase/serverless"
+import dotenv from "dotenv"
+dotenv.config()
+
+const DATABASE_URL = process.env.DATABASE_URL
+export const sql = neon(DATABASE_URL)
+
+export async function initdb() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10, 2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log("database initialized successfully")
+
+    } catch (error)
+    {
+        console.log("Error initialising database", error)
+        process.exit(1)
+    }
+}
